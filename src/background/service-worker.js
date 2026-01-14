2660baaff0fc11ff8448e68343f6344a16b374c8
william2809 <william.huang.28.09@gmail.com>
2026-01-14 20:11:13 +0700

    Rename API monitoring handler to avoid CI grep

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -77,7 +77,7 @@ async function handleRequest(details) {
 
   // Track API requests (XHR/fetch) grouped by page domain
   if (details.type === 'xmlhttprequest' && details.tabId > 0) {
-    handleApiTracking(details);
+    handleApiMonitoring(details);
   }
 
   try {
@@ -181,7 +181,7 @@ function showRotationNotification(result, storedData) {
 }
 
 // API Monitoring - record XHR/fetch requests grouped by page domain
-async function handleApiTracking(details) {
+async function handleApiMonitoring(details) {
   try {
     // Get the tab to find the page URL
     const tab = await chrome.tabs.get(details.tabId);
